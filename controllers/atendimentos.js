74505d86a334f197ccbefe0e67d90dcf39cacd16
XamuAvila <56359366+XamuAvila@users.noreply.github.com>
2021-06-19 22:59:10 -0300

    projetoFinal

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,12 +1,30 @@
+const atendimentos = require('../models/atendimentos');
 const Atendimento = require('../models/atendimentos')
 module.exports = app =>{
     app.get('/atendimentos', (req, res)=>{
-        res.send('Servidor rodando, tudo ok');
+        Atendimento.lista(res);
+    })
+
+    app.get('/atendimentos/:id', (req, res)=>{
+        const id = parseInt(req.params.id);
+        atendimentos.buscaPorId(id, res)
     })
 
     app.post('/atendimentos', (req, res)=>{
         const atendimento = req.body
-        Atendimento.adiciona(atendimento)
-        res.send('Vc ta na rota de atendimentos usando POST')
+        Atendimento.adiciona(atendimento, res)
+    })
+
+    app.patch('/atendimentos/:id', (req, res)=>{
+        const id = parseInt(req.params.id);
+        const valores = req.body
+
+        Atendimento.altera(id, valores, res)
+    })
+
+    app.delete('/atendimentos/:id', (req, res)=>{
+        const id = parseInt(req.params.id);
+
+        Atendimento.deleta(id, res)
     })
 }
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,13 +1,91 @@
+const { response } = require('express');
+const moment = require('moment');
+const { restart } = require('nodemon');
 const conexao = require('../infraestrutura/conexao')
-class Atendimento{
-    adiciona(atendimento){
-        console.log(atendimento)
-        const sql = 'INSERT INTO Atendimentos SET ?' ;
-        conexao.query(sql, atendimento, (erro, resultado)=>{
+class Atendimento {
+    adiciona(atendimento, res) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss');
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss')
+
+        const dataValida = moment(data).isSameOrAfter(dataCriacao);
+        const nomeValido = atendimento.cliente.length >= 5;
+
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: nomeValido,
+                mensagem: "Cliente deve ter pelo menos 5 caracteres"
+            }
+        ];
+
+        const erros = validacoes.filter(campo => !campo.valido);
+        const existemErros = erros.length
+
+        if (existemErros) {
+            res.status(400).json(erros);
+        } else {
+            const atendimentoDatado = { ...atendimento, dataCriacao, data }
+            const sql = 'INSERT INTO Atendimentos SET ?';
+            conexao.query(sql, atendimentoDatado, (erro, resultado) => {
+                if (erro) {
+                    res.status(400).json(erro)
+                } else {
+                    res.status(201).json(resultado)
+                }
+            })
+        }
+    }
+
+    lista(res){
+        const sql = 'SELECT * FROM  atendimentos';
+        conexao.query(sql, (erro, resultados)=>{
             if(erro){
-                console.log(erro)
+                res.status(400).json(erro);
             }else{
-                console.log(resultado)
+                res.status(200).json(resultados);
+            }
+        })
+    }
+
+    buscaPorId(id, res){
+        const sql = `SELECT * FROM atendimentos where id = ${id}`;
+        conexao.query(sql, (erro, resultados)=>{
+            const atendimento = resultados[0];
+            if(erro){
+                res.status(400).json(erro);
+            }else{
+                res.status(200).json(atendimento);
+            }
+        })
+    }
+
+    altera(id, valores, res){
+        const sql = `UPDATE atendimentos SET ? WHERE id=?`;
+        if(valores.data){
+            valores.data = moment(valores.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss')
+        }
+        conexao.query(sql, [valores, id], (erro, resultados)=>{
+            if(erro){
+                res.status(400).json(erro);
+            }else{
+                res.status(200).json({...valores, id});
+            }
+           
+        })
+    }
+
+    deleta(id, res){
+        const sql = `DELETE from atendimentos where id=?`;
+        conexao.query(sql, id, (erro, resultado)=>{
+            if(erro){
+                res.status(400).json(erro);
+            }else{
+                res.status(200).json({id});
             }
         })
     }
